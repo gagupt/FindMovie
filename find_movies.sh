fee1f043bcc6bfc8b3e51a2f491357ee086fc664
Gaurav <guptagaurav890@gmail.com>
2017-04-13 23:57:25 +0530

    language added

--- find_movies.sh
+++ find_movies.sh
@@ -6,4 +6,13 @@ fi
 javac ReadWebsiteData.java
 #echo $1
 java ReadWebsiteData $1> out.txt
-cat out.txt| grep "data-id=\"movies"| cut -d "=" -f4|cut -d "\"" -f2
+
+declare -a x
+declare -a y
+cat out.txt| grep "data-id=\"movies"| cut -d "=" -f4|cut -d "\"" -f2>movies.txt
+cat out.txt | grep "language'>"| cut -d ">" -f2| cut -d "<" -f1>language.txt
+
+paste movies.txt language.txt | column -s $'\t' -t
+
+
+
